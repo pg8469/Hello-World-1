064e7a8e6140a3e75d144f499f3a1e31aa3182c1
Nahatapc <43502247+Nahatapc@users.noreply.github.com>
2018-10-27 03:33:00 +0530

    Hello_World in C

---
+++ Hello_World.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void){
+
+	int data;
+	printf("enter the value of data");
+	scanf("%d",&data);
+	if(data==1){
+		printf("HELLO,World !");
+	}
+	else
+		printf("to print HELLO, World ! please choose data is to be 1");
+	return 0;
+}
+
